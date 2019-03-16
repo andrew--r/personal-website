cd79ab7ed2a9309e770b790844dbd12da4e2ba13
Andrey Romanov <scorpion21.97@gmail.com>
2019-03-16 23:44:47 +0300

    Add RSS feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,22 @@
-const cacheBuster = require('@mightyplow/eleventy-plugin-cache-buster');
+const rss = require('@11ty/eleventy-plugin-rss');
 const { addNunjucksFilters } = require('./eleventy/nunjucks-fitlers');
 const { addNunjucksShortcodes } = require('./eleventy/nunjucks-shortcodes');
+const { addTransforms } = require('./eleventy/transforms');
 
-const outputDirectory = 'build';
+const dir = {
+  input: 'source',
+  output: 'build',
+};
 
 module.exports = (eleventyConfig) => {
-  eleventyConfig.addPlugin(
-    cacheBuster({
-      outputDirectory,
-    }),
-  );
+  eleventyConfig.addPlugin(rss);
   eleventyConfig.addPassthroughCopy('source/assets');
   addNunjucksFilters(eleventyConfig);
   addNunjucksShortcodes(eleventyConfig);
+  addTransforms(eleventyConfig, dir);
 
   return {
-    dir: {
-      input: 'source',
-      output: outputDirectory,
-    },
+    dir,
     templateFormats: ['njk', 'md', 'css', 'png'],
     passthroughFileCopy: true,
   };
