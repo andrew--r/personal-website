f3a73b0662765da61031078bdd3e963cb9bdef1c
Andrey Romanov <scorpion21.97@gmail.com>
2019-05-03 00:29:21 +0300

    Add styles preprocessing

--- .eleventy.js
+++ .eleventy.js
@@ -2,22 +2,23 @@ const rss = require('@11ty/eleventy-plugin-rss');
 const { addNunjucksFilters } = require('./eleventy/nunjucks-fitlers');
 const { addNunjucksShortcodes } = require('./eleventy/nunjucks-shortcodes');
 const { addTransforms } = require('./eleventy/transforms');
+const buildConfig = require('./build-config');
 
 const dir = {
-  input: 'source',
-  output: 'build',
+  input: buildConfig.dir.eleventySource,
+  output: buildConfig.dir.build,
 };
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPlugin(rss);
-  eleventyConfig.addPassthroughCopy('source/assets');
+  eleventyConfig.addPassthroughCopy(`${dir.input}/assets`);
   addNunjucksFilters(eleventyConfig);
   addNunjucksShortcodes(eleventyConfig);
   addTransforms(eleventyConfig, dir);
 
   return {
     dir,
-    templateFormats: ['njk', 'md', 'css', 'png'],
+    templateFormats: ['njk', 'md', 'png'],
     passthroughFileCopy: true,
   };
 };
