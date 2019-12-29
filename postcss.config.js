ee1f4fabfaa0726e4179fb2b5d096a593f5bfe94
Andrey Romanov <me@andreyromanov.com>
2019-12-30 00:35:41 +0300

    Refactor styles a bit

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,9 @@ module.exports = {
   plugins: [
     require('postcss-easy-import')(),
     require('postcss-selector-matches')(),
+    require('postcss-focus-visible')({
+      preserve: false,
+    }),
     require('postcss-nested')(),
     require('autoprefixer')(),
     require('postcss-csso')(),
