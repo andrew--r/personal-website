f912ed4b0ff9200d3f497044583436ef3dfddfb0
Andrey Romanov <me@andreyromanov.com>
2020-03-10 23:39:34 +0300

    Improve articles list design

--- andrew-r.ru/eleventy/nunjucks-shortcodes/index.js
+++ andrew-r.ru/eleventy/nunjucks-shortcodes/index.js
@@ -1,8 +1,10 @@
 const { formatDate } = require('./format-date');
+const { formatShortDate } = require('./format-short-date');
 const { formatNumber } = require('./format-number');
 
 function addNunjucksShortcodes(eleventyConfig) {
   eleventyConfig.addNunjucksShortcode('formatDate', formatDate);
+  eleventyConfig.addNunjucksShortcode('formatShortDate', formatShortDate);
   eleventyConfig.addNunjucksShortcode('formatNumber', formatNumber);
 }
 
--- andreyromanov.com/eleventy/nunjucks-shortcodes/format-short-date.js
+++ andreyromanov.com/eleventy/nunjucks-shortcodes/format-short-date.js
@@ -1,11 +1,10 @@
 const formatter = new Intl.DateTimeFormat('en', {
-  year: 'numeric',
   month: '2-digit',
   day: '2-digit',
 });
 
-function formatDate(date) {
+function formatShortDate(date) {
   return formatter.format(date);
 }
 
-module.exports = { formatDate };
+module.exports = { formatShortDate };
