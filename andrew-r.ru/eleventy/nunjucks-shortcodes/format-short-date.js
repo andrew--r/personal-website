f0423517257d162d90478dbe818dbf01b51a6d11
Andrey Romanov <me@andreyromanov.com>
2020-03-10 23:39:34 +0300

    Improve articles list design

--- andrew-r.ru/eleventy/nunjucks-shortcodes/format-short-date.js
+++ andrew-r.ru/eleventy/nunjucks-shortcodes/format-short-date.js
@@ -1,11 +1,10 @@
 const formatter = new Intl.DateTimeFormat('ru', {
-  year: 'numeric',
   month: '2-digit',
   day: '2-digit',
 });
 
-function formatDate(date) {
+function formatShortDate(date) {
   return formatter.format(date);
 }
 
-module.exports = { formatDate };
+module.exports = { formatShortDate };
