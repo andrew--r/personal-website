ec2d79909b45da224814c62fd5c776a4b927bb47
Andrey Romanov <me@andreyromanov.com>
2020-04-29 22:53:40 +0300

    Add syntax highlighting

--- andreyromanov.com/.eleventy.js
+++ andreyromanov.com/.eleventy.js
@@ -1,4 +1,6 @@
 const rss = require('@11ty/eleventy-plugin-rss');
+const markdownIt = require('markdown-it');
+const hljs = require('highlight.js');
 const { addNunjucksFilters } = require('./eleventy/nunjucks-fitlers');
 const { addNunjucksShortcodes } = require('./eleventy/nunjucks-shortcodes');
 const { addTransforms } = require('./eleventy/transforms');
@@ -9,12 +11,21 @@ const dir = {
   output: buildConfig.dir.build,
 };
 
-module.exports = (eleventyConfig) => {
-  eleventyConfig.addPlugin(rss);
-  eleventyConfig.addPassthroughCopy(`${dir.input}/assets`);
-  addNunjucksFilters(eleventyConfig);
-  addNunjucksShortcodes(eleventyConfig);
-  addTransforms(eleventyConfig, dir);
+module.exports = (config) => {
+  config.setLibrary(
+    'md',
+    markdownIt({
+      highlight: (source, language) =>
+        language ? hljs.highlight(language, source).value : '',
+    }),
+  );
+
+  config.addPlugin(rss);
+  config.addPassthroughCopy(`${dir.input}/assets`);
+
+  addNunjucksFilters(config);
+  addNunjucksShortcodes(config);
+  addTransforms(config, dir);
 
   return {
     dir,
