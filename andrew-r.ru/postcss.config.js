81216e3d9867389e654c3c8c1d2f8447336e0199
Andrey Romanov <me@andreyromanov.com>
2023-03-26 18:10:45 +0100

    Upgrade postcss and other deps

--- andrew-r.ru/postcss.config.js
+++ andrew-r.ru/postcss.config.js
@@ -2,9 +2,6 @@ module.exports = {
   plugins: [
     require('postcss-easy-import')(),
     require('postcss-selector-matches')(),
-    require('postcss-focus-visible')({
-      preserve: false,
-    }),
     require('postcss-nested')(),
     require('autoprefixer')(),
     require('postcss-csso')(),
